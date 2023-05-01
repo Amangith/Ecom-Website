e1ae071c8b3c172ebaed9e471357c769c62ef5b3
Aman <aman89199@gmail.com>
2023-05-01 10:32:32 +0530

    first commit

--- src/modules/Cart/index.js
+++ src/modules/Cart/index.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const Cart = () => {
-  const carts = JSON.parse(localStorage.getItem("cart")) || [];
+  const carts = useMemo(() => JSON.parse(localStorage.getItem("cart")) || [], []);
+
   const navigate = useNavigate()
   const [total, setTotal] = useState(0)
 
